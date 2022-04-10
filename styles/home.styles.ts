3b2c16944b8ec4a862b2291ac70eb6dc2a603d93
Vahid Hasani <vahidhasani09@gmail.com>
2022-04-10 16:37:09 +0430

    Fix dependencies material

---
+++ styles/home.styles.ts
@@ -0,0 +1,9 @@
+import {makeStyles, Theme} from "@mui/material";
+
+const useHomeStyles = makeStyles((theme: Theme) => ({
+    root: {
+        backgroundColor: theme.palette.primary.main
+    }
+}))
+
+export default useHomeStyles;
