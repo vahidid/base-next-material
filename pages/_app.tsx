fa669f15e74685ef5f69fa752c744e15578be4af
Vahid Hasani <vahidhasani09@gmail.com>
2022-04-11 12:50:10 +0430

    Eslint test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
-import type { AppProps } from 'next/app'
-import {ThemeProvider} from "@mui/material";
-import theme from "../styles/theme";
+import { ThemeProvider } from "@mui/material"
+import type { AppProps } from "next/app"
+
+import theme from "../styles/theme"
 
 function MyApp({ Component, pageProps }: AppProps) {
-
-  return <ThemeProvider theme={theme}><Component {...pageProps} /></ThemeProvider>
+    return <Component {...pageProps} />
 }
 
 export default MyApp
